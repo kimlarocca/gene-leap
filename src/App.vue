a38aada4be92913cc546990ab7f1809d952614ee
Kim LaRocca <kim@kimlarocca.com>
2021-01-07 14:21:20 -0500

    removed nav items for now

--- src/App.vue
+++ src/App.vue
@@ -14,118 +14,6 @@
           />
         </router-link>
       </template>
-      <template v-slot:button>
-        <router-link to="investors">
-          <v-button>
-            Investors
-          </v-button>
-        </router-link>
-      </template>
-      <template v-slot:menu>
-        <div>
-          <v-menu
-              layout="right"
-              :primaryNav="[
-                    {
-                      route: '/about',
-                      text: 'About',
-                    },
-                    {
-                      route: '/technology',
-                      text: 'Technology',
-                    },
-                    {
-                      route: '/careers',
-                      text: 'Careers',
-                    },
-                    {
-                      route: '/news',
-                      text: 'News',
-                    },
-                    {
-                      route: '/contact',
-                      text: 'Contact',
-                    }
-                  ]"
-          >
-            <template v-slot:logo>
-              <img
-                  src="/images/logo-gray.jpg"
-                  width="200"
-                  alt="GeneLeap Bioteach logo"
-              />
-            </template>
-            <template v-slot:button>
-              <router-link to="investors">
-                <v-button>
-                  Investors
-                </v-button>
-              </router-link>
-            </template>
-            <template v-slot:social>
-              <ul class="social-links">
-                <social-link
-                    icon="fab fa-facebook-f"
-                    link="https://www.facebook.com"
-                    label="facebook"
-                >
-                </social-link>
-                <social-link
-                    icon="fab fa-twitter"
-                    link="https://www.twitter.com"
-                    label="twitter"
-                >
-                </social-link>
-                <social-link
-                    icon="fab fa-youtube"
-                    link="https://www.youtube.com"
-                    label="youtube"
-                >
-                </social-link>
-                <social-link
-                    icon="fab fa-instagram"
-                    link="https://www.instagram.com"
-                    label="instagram"
-                >
-                </social-link>
-                <social-link
-                    icon="fab fa-linkedin-in"
-                    link="https://www.linkedin.com"
-                    label="linkedin"
-                >
-                </social-link>
-              </ul>
-            </template>
-          </v-menu>
-        </div>
-      </template>
-      <template v-slot:navigation>
-        <secondary-navigation
-            orientation="horizontal"
-            :nav-items="[
-                    {
-                      route: '/about',
-                      text: 'About',
-                    },
-                    {
-                      route: '/technology',
-                      text: 'Technology',
-                    },
-                    {
-                      route: '/careers',
-                      text: 'Careers',
-                    },
-                    {
-                      route: '/news',
-                      text: 'News',
-                    },
-                    {
-                      route: '/contact',
-                      text: 'Contact',
-                    }
-                  ]"
-        />
-      </template>
     </v-header>
     <v-spacer size="hex" />
     <router-view />
@@ -133,36 +21,6 @@
   </div>
   <v-footer
       company-name="GeneLeap Biotech"
-      :navigation="[
-                    {
-                      route: '/',
-                      text: 'Home',
-                    },
-                    {
-                      route: '/about',
-                      text: 'About',
-                    },
-                    {
-                      route: '/technology',
-                      text: 'Technology',
-                    },
-                    {
-                      route: '/careers',
-                      text: 'Careers',
-                    },
-                    {
-                      route: '/news',
-                      text: 'News',
-                    },
-                    {
-                      route: '/contact',
-                      text: 'Contact',
-                    },
-                    {
-                      route: '/investors',
-                      text: 'Investors',
-                    }
-                  ]"
   >
     <template v-slot:blurb>
       <p>Unless otherwise specified, all product names appearing on this Internet site are trademarks owned by or licensed to Luye Boston R&D IIC, its subsidiaries or affiliates. No use of any Luye Boston R&D IIC trademark, trade name, or trade dress on this site may be made without the prior written authorization of Luye Boston R&D IIC except to identify the product or services of the company. This Luye Boston R&D IIC website focusing on science is intended for the use of U.S. healthcare professionals any sciences.</p>
@@ -171,22 +29,14 @@
 </template>
 
 <script>
-import SecondaryNavigation from 'vue-evolve/src/components/SecondaryNavigation'
-import SocialLink from 'vue-evolve/src/components/SocialLink'
-import VButton from 'vue-evolve/src/components/VButton'
 import VHeader from 'vue-evolve/src/components/VHeader'
-import VMenu from 'vue-evolve/src/components/VMenu'
 import VSpacer from 'vue-evolve/src/components/VSpacer'
 import VFooter from 'vue-evolve/src/components/VFooter'
 
 export default {
   name: 'GeneLeap',
   components: {
-    SecondaryNavigation,
-    SocialLink,
-    VButton,
     VHeader,
-    VMenu,
     VSpacer,
     VFooter
   }
