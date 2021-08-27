f7470ebd00e444d692d3c656579cb98b91152860
Kim LaRocca <kim@kimlarocca.com>
2021-08-27 14:14:26 -0400

    tim's updates 8-27-21

--- src/views/About.vue
+++ src/views/About.vue
@@ -18,25 +18,23 @@
         <h3>Executive Team</h3>
         <div class="l-grid l-grid--2up l-grid--large-gutters">
           <biography-card
-              image="/images/team/ekambar.jpg"
-              altText="Ekambar Kandimalla, VP of mRNA chemistry and oligo research"
-              name="Ekambar Kandimalla, VP of mRNA chemistry and oligo research"
-              layout="horizontal"
+            image="/images/team/wenzhong.jpg"
+            altText="Dr. Wenzhong Chen, CEO"
+            name="Dr. Wenzhong Chen, CEO"
+            layout="horizontal"
           >
             <slot>
               <ul>
-                <li>Previously CSO at Exicure; VP Idera;</li>
-                <li>World’s leading scholar on nucleic acid chemistry;</li>
-                <li>Broad experiences in siRNA and SNA therapeutic development;</li>
-                <li>Moved multiple candidates into clinical trials.</li>
+                <li>20+year experience in scientific research management of pharmaceutical enterprises;</li>
+                <li>Expert of liposomal drug development and industrialization.</li>
               </ul>
             </slot>
           </biography-card>
           <biography-card
-              image="/images/team/tim.jpg"
-              altText="Tim Maguire, Head of Business Development"
-              name="Tim Maguire, Head of Business Development"
-              layout="horizontal"
+            image="/images/team/tim.jpg"
+            altText="Tim Maguire, CBO, Head of Business Development"
+            name="Tim Maguire, CBO, Head of Business Development"
+            layout="horizontal"
           >
             <slot>
               <ul>
@@ -47,29 +45,31 @@
             </slot>
           </biography-card>
           <biography-card
-              image="/images/team/sun.jpg"
-              altText="Kaoxiang Sun, VP"
-              name="Kaoxiang Sun, VP"
-              layout="horizontal"
+            image="/images/team/tomhuang.jpg"
+            altText="Tom Huang, COO"
+            name="Tom Huang, COO"
+            layout="horizontal"
           >
             <slot>
               <ul>
                 <li>VP at Luye Life sciences;</li>
                 <li>15+ years experience of drug delivery system development in pharmaceutical industry. Led the development of multiple products and some of them have been approved or in clinical trials phase;</li>
-                <li>PhD from China Pharmaceutical University.</li>
+                <li>PhD from University of Akron in advance drug delivery.</li>
               </ul>
             </slot>
           </biography-card>
           <biography-card
-              image="/images/team/wenzhong.jpg"
-              altText="Wenzhong Chen, Deputy General Manager"
-              name="Wenzhong Chen, Deputy General Manager"
-              layout="horizontal"
+            image="/images/team/ekambar.jpg"
+            altText="Ekambar Kandimalla, VP of mRNA chemistry and oligo research"
+            name="Ekambar Kandimalla, VP of mRNA chemistry and oligo research"
+            layout="horizontal"
           >
             <slot>
               <ul>
-                <li>20+year experience in scientific research management of pharmaceutical enterprises;</li>
-                <li>Expert of liposomal drug development and industrialization.</li>
+                <li>Previously CSO at Exicure; VP Idera;</li>
+                <li>World’s leading scholar on nucleic acid chemistry;</li>
+                <li>Broad experiences in siRNA and SNA therapeutic development;</li>
+                <li>Moved multiple candidates into clinical trials.</li>
               </ul>
             </slot>
           </biography-card>
@@ -82,28 +82,37 @@
       <h3>Board of Advisors</h3>
       <div class="l-grid l-grid--2up l-grid--large-gutters">
         <biography-card
-            image="/images/team/guangping.jpg"
-            altText="Prof. Guangping Gao"
-            name="Prof. Guangping Gao"
-            layout="horizontal"
-        >
-          <slot>
-            <p>Co-Director, Li Weibo Institute for Rare Diseases Research, Director, Horae Gene Therapy Center and Viral Vector Core, Professor in the University of Massachusetts Medical School.</p>
-            <v-spacer />
-            <p>Dr. Gao played a key role in the discovery and characterization of new family of adeno-associated virus (AAV) serotypes, which was instrumental in reviving the gene therapy field. Dr. Gao has published 250+ research papers, holds 131 patents with 221 more pending. Dr. Gao was a co-founder of Voyager Therapeutics.
-            </p>
-          </slot>
-        </biography-card>
-        <biography-card
-            image="/images/team/herzog.jpg"
-            altText="Prof. Roland W. Herzog"
-            name="Prof. Roland W. Herzog"
-            layout="horizontal"
+          image="/images/team/herzog.jpg"
+          altText="Prof. Roland W. Herzog"
+          name="Prof. Roland W. Herzog"
+          layout="horizontal"
         >
           <slot>
             <p>Professor of Pediatrics, Riley Children‘s Foundation Professor of Immunology, and Director of the Gene and Cell Therapy Program. World’s leading exper in gene therapy and immune tolerance for hemophilia and in AAV vectors.</p>
             <v-spacer />
             <p>Dr. Herzog’s research was recognized by awards from American Society of Gene and Cell Therapy, the National Hemophilia Foundation, the Bayer Hemophilia Program, and the University of Florida Research Foundation, among others. Dr. Herzog served on SAB of Sparks Therapeutics.</p>
+          </slot>
+        </biography-card>
+        <biography-card
+          image="/images/team/placeholder.png"
+          altText="Dr. Bob Langer"
+          name="Dr. Bob Langer"
+          layout="horizontal"
+        >
+          <slot>
+            <p>Dr. Langer’s pioneering work in the study and development of polymers to deliver drugs, particularly macromolecules, has lead to countless breakthroughs in the filed of biotechnology, and has led to the advancement of countless therapeutics.</p>
+            <v-spacer />
+            <p>Dr. Langer will advise on the development of delivery platforms for non-messenger RNA therapeutic approaches.</p>
+          </slot>
+        </biography-card>
+        <biography-card
+          image="/images/team/placeholder.png"
+          altText="Dr. Drew Weissman"
+          name="Dr. Drew Weissman"
+          layout="horizontal"
+        >
+          <slot>
+            <p>Coming soon.</p>
           </slot>
         </biography-card>
       </div>
